d7d768dd9743b0dd3dd9721bfd84349346bb58e5
Wilson Nyumbe <wilsonnyumbe99@gmail.com>
2024-02-06 08:52:20 +0300

    created an intance for the BonusCalculator class

--- WILSON A89145/SOLID PRINCIPLES/Refactored.py
+++ WILSON A89145/SOLID PRINCIPLES/Refactored.py
@@ -34,3 +34,7 @@ if __name__ == "__main__":
     report_generator = Report()
     report_generator.generate_report(manager)
     report_generator.generate_report(developer)
+
+    bonus_calculator = BonusCalculator()
+    manager_bonus = bonus_calculator.calculate_bonus(manager)
+    developer_bonus = bonus_calculator.calculate_bonus(developer)
