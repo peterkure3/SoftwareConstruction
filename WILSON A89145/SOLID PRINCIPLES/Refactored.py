2977236a9c58a04222616a21e3d3fe2112fcfb8a
Wilson Nyumbe <wilsonnyumbe99@gmail.com>
2024-02-06 08:44:53 +0300

    added the refactored Developer class

--- WILSON A89145/SOLID PRINCIPLES/Refactored.py
+++ WILSON A89145/SOLID PRINCIPLES/Refactored.py
@@ -21,3 +21,8 @@ class Manager(Employee):
     
     def manage_team(self):
         print(f"{self.name} is managing the team.")
+
+class Developer(Employee):
+    
+    def code_review(self):
+        print(f"{self.name} is conducting a code review.")
