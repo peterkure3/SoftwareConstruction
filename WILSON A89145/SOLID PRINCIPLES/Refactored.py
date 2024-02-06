9e9064f3ad1231d2a2b806663fdc52d3ac73af96
Wilson Nyumbe <wilsonnyumbe99@gmail.com>
2024-02-06 08:54:03 +0300

    printed a manager's and developer's bonus

--- WILSON A89145/SOLID PRINCIPLES/Refactored.py
+++ WILSON A89145/SOLID PRINCIPLES/Refactored.py
@@ -38,3 +38,7 @@ if __name__ == "__main__":
     bonus_calculator = BonusCalculator()
     manager_bonus = bonus_calculator.calculate_bonus(manager)
     developer_bonus = bonus_calculator.calculate_bonus(developer)
+
+    print(f"Manager Bonus: ${manager_bonus}")
+    print(f"Developer Bonus: ${developer_bonus}")
+
