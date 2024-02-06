e936ede20b44596e71d2d24357ff30c94bec6f60
Wilson Nyumbe <wilsonnyumbe99@gmail.com>
2024-02-06 08:56:01 +0300

    called the manage_team method and developer_review method

--- WILSON A89145/SOLID PRINCIPLES/Refactored.py
+++ WILSON A89145/SOLID PRINCIPLES/Refactored.py
@@ -42,3 +42,6 @@ if __name__ == "__main__":
     print(f"Manager Bonus: ${manager_bonus}")
     print(f"Developer Bonus: ${developer_bonus}")
 
+    manager.manage_team()
+    developer.code_review() 
+
