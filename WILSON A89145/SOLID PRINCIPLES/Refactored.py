97d1b363b70758df9ef6bbf521d5ccdd4fe6364d
Wilson Nyumbe <wilsonnyumbe99@gmail.com>
2024-02-06 08:39:11 +0300

    added refactored Report class

--- WILSON A89145/SOLID PRINCIPLES/Refactored.py
+++ WILSON A89145/SOLID PRINCIPLES/Refactored.py
@@ -2,3 +2,10 @@ class Employee:
     def __init__(self, name, role):
         self.name = name
         self.role = role
+
+class Report:
+    def generate_report(self, employee):
+        if employee.role == "Manager":
+           print(f"Manager Report: {manager.name}")
+        elif employee.role == "Developer":
+             print(f"Developer Report: {developer.name}")
