0fe49f7d67a64d91d84d6a3af0d65d09761d793a
MUTUA BRIAN <92850581+Mutua303@users.noreply.github.com>
2024-02-07 14:10:16 +0300

    Instantiate BonusCalculator class and calculate bonuses for Manager and Developer instances

--- BRIAN MUTUA A94174/Refactor.py
+++ BRIAN MUTUA A94174/Refactor.py
@@ -33,4 +33,8 @@ if __name__ == "__main__":
 
     report_generator = Report()
     report_generator.generate_report(manager)
-    report_generator.generate_report(developer)                
+    report_generator.generate_report(developer)
+
+    bonus_calculator = BonusCalculator()
+    manager_bonus = bonus_calculator.calculate_bonus(manager)
+    developer_bonus = bonus_calculator.calculate_bonus(developer)                
